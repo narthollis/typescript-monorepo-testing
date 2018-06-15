0596e643c1d2e9080b4547674b2afb31ff0f684b
Nicholas Steicke <nicholas.steicke@narthollis.net>
2018-06-15 22:20:58 +0930

    more setup

--- Web/packages/x-cli/src/index.ts
+++ Web/packages/x-cli/src/index.ts
@@ -1,3 +1,7 @@
+import path from 'path';
+
 import { foo } from 'x-core';
 
-console.log(`a.len - b.len = ${foo(process.argv[1], process.argv[2])}`)
+const args = process.argv.slice(process.argv.findIndex(arg => path.normalize(arg) === __filename) + 1)
+
+console.log(`a.len - b.len = ${foo(args[0], args[1])}`)
